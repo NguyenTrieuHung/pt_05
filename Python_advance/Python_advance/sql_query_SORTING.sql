7ee660e5ea4261225cd375bac6b7ee87cdb87b54
NguyenTrieuHung <nguyentrieuhung2022@gmail.com>
2022-09-19 19:28:25 +0700

    q

---
+++ Python_advance/Python_advance/sql_query_SORTING.sql
@@ -0,0 +1,20 @@
+-- B. SQLite Restricting and Sorting Data 
+
+-- 1. Write a query to display the names (first_name, last_name) and salary for all employees whose salary is not in the range $10,000 through $15,000.
+
+-- 2. Write a query to display the names (first_name, last_name) and department ID of all employees in departments 30 or 100 in ascending alphabetical order by department ID. 
+
+-- 3. Write a query to display the names (first_name, last_name) and salary for all employees whose salary is not in the range $10,000 through $15,000 and are in department 30 or 100.
+
+-- 4. Write a query to display the first_name of all employees who have both an "b" and "c" in their first name.
+
+-- 5. Write a query to display the last name, job, and salary for all employees whose job is that of a Programmer or a Shipping Clerk, and whose salary is not equal to $4,500, $10,000, or $15,000.
+
+-- 6. Write a query to display the last names of employees whose names have exactly 6 characters. 
+
+
+-- 7. Write a query to display the last names of employees having 'e' as the third character.
+
+-- 8. Write a query to display the jobs/designations available in the employees table. 
+
+-- 9. Write a query to display the names (first_name, last_name), salary and PF (15% of salary) of all employees. 
